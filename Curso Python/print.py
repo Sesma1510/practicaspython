de3021cfe475c3f74877d5baf6b5c6bdfbb2ce14
Eduardo S <sesma.2012@hotmail.com>
2022-11-08 01:24:20 -0600

    Mis practicas de Python

---
+++ Curso Python/print.py
@@ -0,0 +1,21 @@
+# Ejemplos de la funcion print()
+
+# print('Hola mundo')
+# print('Hola mundo', 'otra vez')
+# print('Son las', 9, 'de la mañana')
+# print('El resultado de 3 * 4 es:', 3*4)
+
+# # Ejemplos de cadenas formateadas
+# print('El numero 15 en sistema decimal es %d, en sistema octal es %o, y en sistema hexadecimal es %x' % (15, 15, 15))
+
+# pi = 3.14192
+# r = 5
+# print(f'El radio de un circulo es {r} y el area de un circulo es {pi * r ** 2:.2f}')
+
+# # Impresion de caracteres especiales
+# print('La letra beta es: \n\t \u03B2')
+
+# Caracteres de escape
+print('Hola mundo', end = ' ')
+print('otra vez', end = '\t')
+print('y otra vez')
---
+++ Curso Python/variables.py
@@ -0,0 +1,28 @@
+# Aqui hay una variable de tipo entero
+numero = 147
+print(numero) # Esta linea sirve para imprimir la variable numero
+print(type(numero))
+
+# Redefinimos la variable numero
+numero = 147.5
+print(numero)
+print(type(numero))
+
+# Aqui hay una variable de tipo complejo
+complejo = 1 + 2j
+print(complejo)
+print(type(complejo))
+
+# Aqui hay una variable de tipo string
+palabra = 'Hola'
+print(palabra)
+print(type(palabra))
+
+# Aqui hay una variable de tipo bool
+bandera = True
+print(type(bandera))
+
+# Aqui hay una variable de tipo Null
+vacio = None
+print(vacio)
+print(type(vacio))
