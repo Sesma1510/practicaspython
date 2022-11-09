ed31f76c4bb80ed0fc360b4e296327c75106e61d
Eduardo S <sesma.2012@hotmail.com>
2022-11-08 21:00:33 -0600

    Commit Inicial

---
+++ Curso Python/Ejercicios_Modulos/holamundo.py
@@ -0,0 +1,4 @@
+print('Hola mundo')
+
+print('Este es mi primer programa en python')
+input()
---
+++ Curso Python/Ejercicios_Modulos/operadores.py
@@ -0,0 +1,39 @@
+# # Operadores aritmeticos
+# print(2 + 2)   # Suma
+# print(4 - 2)   # Rest
+# print(3 * 7)   # Multiplicacion
+# print(15 / 2)  # Division
+# print(11 % 4)  # Modulo
+# print(11 // 4) # Division de piso
+# print(2 ** 3)  # Potenciacion
+
+# # Operadores en cadenas
+# print('Hola' + ' mundo')    # concatenacion
+# print('Hola' * 3)           # repeticion
+# print('Hola' + 'mundo' * 3) # concatenacion y repeticion
+
+# # Operadores de comparacion
+# print('Hola' == 'hola') # Igual que
+# print('Hola' != 'hola') # Distinto que
+# print(3 < 11)           # Menor o mayor que
+# print(11 >= 10)         # Mayor o igual que
+# print(10 <= 10)         # Menor o igual que
+
+# # Operadores de existencia
+# print('ola' in 'Hola')      # De existencia
+# print('ola' not in 'Hola')  # De inexistencia
+
+# # Operadores Booleanos
+# print(True or False)  # or verifica que se cumpla solo una de las dos condiciones
+# print(True and False) # and verifica que se cumplan las dos condiciones
+
+# # Operadores de asignacion
+# x = 1   # Operador de asignacion estandar
+# x += 2  # Operador de asignacion suma
+# x *= 3  # Operador de asignacion multiplicacion  
+# x %= 4  # Operador de asignacion modulo
+# print(x)
+
+# # Jerarquia de operaciones 
+# x = 6 * 5 + 9 / 2 ** 4
+# print(x)
